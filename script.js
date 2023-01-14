53ad114812dbada6facae460ecd61be3acb0d555
Anirban-dash <68104326+Anirban-dash@users.noreply.github.com>
2023-01-14 23:36:07 +0530

    V1.0

--- script.js
+++ script.js
@@ -1,47 +1,169 @@
-var token='90932417|-31949269994274047|90955712';
-var dbname='SCHOOL-DB';
-var relation="STUDENT-TABLE";
+var token = '90932417|-31949269994274047|90955712';
+var dbname = 'SCHOOL-DB';
+var relation = "STUDENT-TABLE";
+function resetForm() {
+    if (!$("#roll").prop("disabled")) {
+        $("#roll").val('')
+    }
+    $("#name").val('');
+    $("#cls").val('');
+    $("#dob").val('');
+    $("#addr").val('');
+    $("#doe").val('');
+}
 function disableAll() {
     $("#roll").focus();
     $("#name").prop("disabled", true);
     $("#cls").prop("disabled", true);
     $("#dob").prop("disabled", true);
+    $("#addr").prop("disabled", true);
     $("#doe").prop("disabled", true);
     $("#save").prop("disabled", true);
     $("#update").prop("disabled", true);
     $("#reset").prop("disabled", true);
 }
 disableAll();
-function findRoll(ele){
-    var roll=ele.value;
-    var obj={
-        Roll_No:roll
+function findRoll(ele) {
+    var roll = ele.value;
+    var obj = {
+        Roll_No: roll
     };
-    var jsnobj=JSON.stringify(obj);
-    var request=createGET_BY_KEYRequest(token,dbname,relation,jsnobj);
-    jQuery.ajaxSetup({async: false});
-    var res=executeCommand(request,"/api/irl");
-    jQuery.ajaxSetup({async: true});
-    if(res.status=400){
+    var jsnobj = JSON.stringify(obj);
+    var request = createGET_BY_KEYRequest(token, dbname, relation, jsnobj);
+    jQuery.ajaxSetup({ async: false });
+    var res = executeCommand(request, "/api/irl");
+    jQuery.ajaxSetup({ async: true });
+    if (res.status == 400) {
         $("#name").prop("disabled", false);
         $("#name").focus();
         $("#cls").prop("disabled", false);
         $("#dob").prop("disabled", false);
+        $("#addr").prop("disabled", false);
         $("#doe").prop("disabled", false);
         $("#save").prop("disabled", false);
         $("#reset").prop("disabled", false);
-    }else{
+    } else {
+        $("#name").prop("disabled", false);
         $("#roll").prop("disabled", true);
-        var data = JSON.parse(res);
-        $("#name").val(data.Full_name);
+        $("#cls").prop("disabled", false);
+        $("#dob").prop("disabled", false);
+        $("#addr").prop("disabled", false);
+        $("#doe").prop("disabled", false);
+        $("#reset").prop("disabled", false);
+        $("#save").prop("disabled", true);
+        $("#update").prop("disabled", false);
+        // console.log(res);
+        var data = JSON.parse(res.data).record;
+        // console.log(data);
+        $("#name").val(data.Full_Name);
         $("#cls").val(data.Class);
-        $("#dob").val(data.Birth_date);
+        $("#dob").val(data.Birth_Date);
+        $("#addr").val(data.Address);
+        $("#doe").val(data.Enrollment_Date);
     }
 }
-function saveData(){
-    var roll=$("#roll").val();
-    var name=$("#name").val()
-    var cls=$("#cls").val()
-    var dob=$("#dob").val()
-    var doe=$("#doe").val()
+function saveData() {
+    var roll = $("#roll").val();
+    var name = $("#name").val()
+    var cls = $("#cls").val();
+    var dob = $("#dob").val();
+    var addr = $("#addr").val();
+    var doe = $("#doe").val();
+    if(roll==''){
+        $("#roll").focus();
+        return;
+    }
+    if(name==''){
+        $("#name").focus();
+        return;
+    }if(cls==''){
+        $("#cls").focus();
+        return;
+    }if(dob==''){
+        $("#dob").focus();
+        return;
+    }if(addr==''){
+        $("#addr").focus();
+        return;
+    }if(doe==''){
+        $("#doe").focus();
+        return;
+    }
+    var obj = {
+        Roll_No: roll,
+        Full_Name: name,
+        Class: cls,
+        Birth_Date: dob,
+        Address: addr,
+        Enrollment_Date: doe
+    };
+    var jsonobj = JSON.stringify(obj);
+    var req = createPUTRequest(token, jsonobj, dbname, relation);
+    jQuery.ajaxSetup({ async: false });
+    var res = executeCommand(req, "/api/iml");
+    jQuery.ajaxSetup({ async: true });
+    disableAll();
+}
+function createSETRequest(token, jsonStr, dbName, relName, type, primaryKey, uniqueKeys, foreignKeys) {
+    if (type === undefined) {
+        type = "DEFAULT";
+    }
+    var req = {
+        token: token,
+        cmd: "SET",
+        dbName: dbName,
+        rel: relName,
+        type: type,
+        jsonStr: JSON.parse(jsonStr)
+    };
+    if (primaryKey !== undefined) {
+        req.primaryKey = primaryKey;
+    }
+    if (uniqueKeys !== undefined) {
+        req.uniqueKeys = uniqueKeys;
+    }
+    if (foreignKeys !== undefined) {
+        req.foreignKeys = foreignKeys;
+    }
+    req = JSON.stringify(req);
+    return req;
+}
+
+function updateData(){
+    var roll = $("#roll").val();
+    var name = $("#name").val()
+    var cls = $("#cls").val();
+    var dob = $("#dob").val();
+    var addr = $("#addr").val();
+    var doe = $("#doe").val();
+    if(name==''){
+        $("#name").focus();
+        return;
+    }if(cls==''){
+        $("#cls").focus();
+        return;
+    }if(dob==''){
+        $("#dob").focus();
+        return;
+    }if(addr==''){
+        $("#addr").focus();
+        return;
+    }if(doe==''){
+        $("#doe").focus();
+        return;
+    }
+    var obj = {
+        Roll_No: roll,
+        Full_Name: name,
+        Class: cls,
+        Birth_Date: dob,
+        Address: addr,
+        Enrollment_Date: doe
+    };
+    var jsonobj = JSON.stringify(obj);
+    var req=createSETRequest(token,jsonobj,dbname,relation,'UPDATE','Roll_No');
+    jQuery.ajaxSetup({ async: false });
+    var res = executeCommand(req, "/api/iml/set");
+    jQuery.ajaxSetup({ async: true });
+    disableAll();
 }
